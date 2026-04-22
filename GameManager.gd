856304c0accb9da536cfd454c3dc4b5bcd489d26
ybchape <yamilachl18@gmail.com>
2026-04-22 18:09:59 -0300

    Agrege un script al enemigo
    genere métodos para preparar el combate utilizando el autoload, otro para dar las recompensas

--- GameManager.gd
+++ GameManager.gd
@@ -1,4 +1,6 @@
 extends Node
+var enemigo_actual_datos = {}
+var frenesi_actual: float = 0.0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +11,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func actualizar_frenesi(valor):
+	frenesi_actual = clamp(frenesi_actual + valor, 0, 100)
+	print("Frenesí biológico en: ", frenesi_actual)
