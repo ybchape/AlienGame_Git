8d55b906d9e5687e9957ee9daf2f46f5833bb5b0
Coral03 <coralgalvan03@gmail.com>
2026-04-25 15:45:54 -0300

    Creacion de del player y físicas básicas
    Se crearon los nodos básicos para el movimiento del player, con sus fisicas correspondientes. Se agrego un script para el movimiento del mismo. Además se crearon las escenas "main" y "plataformas".

---
+++ scenes/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+@export_group("Movimiento")
+@export var walk_speed := 260.0
+
+@export_group("Interacción")
+@export var break_distance := 50.0 # distancia para romper bloque
+
+func _physics_process(delta: float) -> void:
+	handle_movement(delta)
+	handle_interaction()
+	move_and_slide()
+
+ # it is not currently in use
+func handle_movement(_delta: float) -> void: 
+	# obtener dirección en x e y
+	var _direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
+func handle_interaction() -> void:
+	if Input.is_action_just_pressed("break"): 
+		break_block()
+
+func break_block() -> void:
+	print("trying to break block") 
