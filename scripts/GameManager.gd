48268553dd62a8bcf7b8b5dfe1800edb42e7276b
Coral03 <coralgalvan03@gmail.com>
2026-05-05 20:23:20 -0300

    Add funcion de romper bloques en GM

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -8,11 +8,13 @@ var frenesi_actual: float = 0.0
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 # Función para que cualquier objeto pueda alterar el frenesí
 func actualizar_frenesi(valor):
 	frenesi_actual = clamp(frenesi_actual + valor, 0, 100)
 	print("Frenesí biológico en: ", frenesi_actual)
+	
+ # Funcion global para cavar/romper bloques
+func romper_bloque(player_position: Vector2, direccion: String):
+	print("Player cavando desde:", player_position)
+	print ("Dirección", direccion)
+	
