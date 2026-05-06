51bf3c49d57a38f1b7e6de550609461e5cf4c26b
ybchape <yamilachl18@gmail.com>
2026-05-06 15:50:42 -0300

    Nueva rama para el combate y agrege un mazo inicial

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -2,11 +2,26 @@ extends Node
 #Guarda los datos del enemigo que tocaste para que la pantalla de combate sepa qué mostrar
 var enemigo_actual_datos = {}
 var frenesi_actual: float = 0.0
+var vida_jugador: int = 80
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
+#El mazo inicial que va a tener el jugador
+var mazo_jugador = [
+	{"nombre": "Golpe de Chatarra", "tipo": "Ataque", "coste": 1, "daño": 6, "escudo": 0, "roba": 0},
+	{"nombre": "Golpe de Chatarra", "tipo": "Ataque", "coste": 1, "daño": 6, "escudo": 0, "roba": 0},
+	{"nombre": "Golpe de Chatarra", "tipo": "Ataque", "coste": 1, "daño": 6, "escudo": 0, "roba": 0},
+	{"nombre": "Golpe de Chatarra", "tipo": "Ataque", "coste": 1, "daño": 6, "escudo": 0, "roba": 0},
+	{"nombre": "Escudo de Emergencia", "tipo": "Capacidad", "coste": 1, "daño": 0, "escudo": 5, "roba": 0},
+	{"nombre": "Escudo de Emergencia", "tipo": "Capacidad", "coste": 1, "daño": 0, "escudo": 5, "roba": 0},
+	{"nombre": "Escudo de Emergencia", "tipo": "Capacidad", "coste": 1, "daño": 0, "escudo": 5, "roba": 0},
+	{"nombre": "Escudo de Emergencia", "tipo": "Capacidad", "coste": 1, "daño": 0, "escudo": 5, "roba": 0},
+	{"nombre": "Instinto de Presa", "tipo": "Ataque", "coste": 2, "daño": 10, "escudo": 0, "roba": 0},
+	{"nombre": "Análisis de Bioma", "tipo": "Capacidad", "coste": 0, "daño": 0, "escudo": 0, "roba": 1}
+]
 
 # Función para que cualquier objeto pueda alterar el frenesí
 func actualizar_frenesi(valor):
