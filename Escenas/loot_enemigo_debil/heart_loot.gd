6c70c1d2bf45f94c2be7e81fa32cd0c66009869d
ybchape <yamilachl18@gmail.com>
2026-06-08 23:12:38 -0300

    Se cambio los valores de recompensa de vida por cada enemigo Debil o medio

--- Escenas/loot_enemigo_debil/heart_loot.gd
+++ Escenas/loot_enemigo_debil/heart_loot.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+var cantidad_curacion:float = 5.0 
+
 func _ready() -> void:
 	# conecta la señal de colisión
 	body_entered.connect(_on_body_entered)
@@ -9,7 +11,7 @@ func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
 		# accede directamente a la vida del GameManager
 		# clamp para no pasarse de la vida maxima (100)
-		RunManager.run_data.vida_jugador = clamp(RunManager.run_data.vida_jugador + 5, 0, RunManager.run_data.vida_maxima)
+		RunManager.run_data.vida_jugador = clamp(RunManager.run_data.vida_jugador + cantidad_curacion, 0, RunManager.run_data.vida_maxima)
 		
-		print("Corazón recolectado.", RunManager.run_data.vida_jugador)
+		print("Corazón recolectado. Curación recibida: +", cantidad_curacion, " | Vida actual: ", RunManager.run_data.vida_jugador)
 		queue_free() # desaparece el corazon
