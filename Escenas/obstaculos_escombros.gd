4a4e9af8f2695e80295fab1f646f0465ab144db9
luciamalena <luciamalena20@gmail.com>
2026-06-24 12:31:12 -0300

    Cambios en el puto texto de la escena principal

--- Escenas/obstaculos_escombros.gd
+++ Escenas/obstaculos_escombros.gd
@@ -1,11 +1,15 @@
 extends StaticBody2D
 
+@export var mostrar_texto: bool = true
+@export var texto_personalizado: String = "Camino bloqueado.\n Derrota al boss"
+# Esta variable va a buscar el Label único que está en la escena principal
+# Usamos absolute path o % si es un nodo único, pero la forma más segura en Godot 4 es buscarlo en el padre:
+@onready var label_principal = get_node_or_null("/root/Escena Principal/TextoInterfazPrincipal")
 @onready var area_deteccion = $CollisionShape2D/Area2D
-@onready var texto_interfaz = $Label
+#@onready var texto_interfaz = $Label
 
 func _ready():
-	# Escondemos el mensaje de texto al arrancar el mapa
-	texto_interfaz.visible = false
+	
 	
 	# COMPROBACIÓN INSTANTÁNEA:
 	# Si en tu GameManager ya marcaste que el camino corto está desbloqueado,
@@ -19,9 +23,16 @@ func _ready():
 
 func _on_body_entered(body):
 	# Comparamos con "player" o tu grupo para saber si es tu astronauta
+	#if body.name == "player" or body.is_in_group("player"):
+		#texto_interfaz.visible = true
 	if body.name == "player" or body.is_in_group("player"):
-		texto_interfaz.visible = true
+		if label_principal != null:
+			label_principal.text = texto_personalizado
+			label_principal.visible = true
 
 func _on_body_exited(body):
+	#if body.name == "player" or body.is_in_group("player"):
+		#texto_interfaz.visible = false
 	if body.name == "player" or body.is_in_group("player"):
-		texto_interfaz.visible = false
+		if label_principal != null:
+			label_principal.visible = false
